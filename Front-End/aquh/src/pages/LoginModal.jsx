75394bd4481086a6c48de4bd7dbfe14ce7da9fec
ninnistic <parkninni@icloud.com>
2023-07-30 17:31:54 +0900

    Feat : 로그인 구현(with mocking API)

---
+++ Front-End/aquh/src/pages/LoginModal.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import LoginForm from '../users/LoginForm'
+import { login } from '../api/api.service'
+
+export default function LoginModal() {
+
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [accessToken, setAccessToken] = useState(null);
+
+    const performLogin = (formData) => {
+        const response = login(formData)
+            .then(response => {
+                setAccessToken(response.data.access_token)
+            })
+            .catch(error => { 
+                setErrorMessage(error.response.data.message)
+            })
+    };
+
+    return <>
+        {errorMessage && <div className="error-message">Oh fuck! {errorMessage}</div>}
+        {accessToken && <p>You're logged in with this token: {accessToken}</p>}
+        <LoginForm onLogin={(formData) => performLogin(formData)} />
+    </>
+}
---
+++ Front-End/aquh/src/stories/LoginModal.stories.jsx
@@ -0,0 +1,10 @@
+import LoginModal from '../pages/LoginModal';
+import mock from '../api/api.mock';
+
+export default {
+  component: LoginModal,
+};
+
+export const Primary = {
+  render: () => <LoginModal />,
+};
