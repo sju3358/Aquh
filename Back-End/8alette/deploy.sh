71ffe19654092f1dd9df0737a609d9ca2e28cee5
김재원 <kjwkjw1104@gmail.com>
2023-07-31 10:05:18 +0900

    Update Back-End/8alette/deploy.sh

--- Back-End/8alette/deploy.sh
+++ Back-End/8alette/deploy.sh
@@ -2,13 +2,13 @@ echo '빌드 시작'
 ./gradlew build
 
 echo '도커파일 이미지 빌드'
-sudo docker build -t springbootapp .
+docker build -t springbootapp .
 
 echo '컨테이너 중지'
-sudo docker stop springbootapp
+docker stop springbootapp
 
 echo '컨테이너 삭제'
-sudo docker rm springbootapp
+docker rm springbootapp
 
 echo '컨테이너 실행'
-sudo docker run -p 8080:8080 --name springbootapp --network ubuntu_default -d springbootapp
+docker run -p 8080:8080 --name springbootapp --network ubuntu_default -d springbootapp
