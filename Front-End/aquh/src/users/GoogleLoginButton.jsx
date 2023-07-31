c8ba54806758feb67cf3d258fcc7b1108b35e9b2
ninnistic <parkninni@icloud.com>
2023-07-31 21:48:20 +0900

    Add : 구글, 네이버 로그인 버튼 생성

---
+++ Front-End/aquh/src/users/GoogleLoginButton.jsx
@@ -0,0 +1,10 @@
+import { RiGoogleFill } from 'react-icons/ri'
+
+
+export default function GoogleLoginButton() {
+return (
+  <>
+    <RiGoogleFill size='17' style={{ marginRight: '8px'}} />
+  </>
+);
+}
---
+++ Front-End/aquh/src/users/NaverLoginButton.jsx
@@ -0,0 +1,10 @@
+import { SiNaver } from 'react-icons/si';
+
+
+export default function NaverLoginButton() {
+return (
+  <>
+    <SiNaver size='17' style={{ marginRight: '8px'}} />
+  </>
+);
+}
