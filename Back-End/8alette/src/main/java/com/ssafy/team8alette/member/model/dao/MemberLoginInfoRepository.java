e2366ac65541e7fbc77bea5694f21cac645b43c4
sju3358 <tjdfkr011.dev@gmail.com>
2023-07-31 13:53:54 +0900

    Fix : 프로젝트 폴더 정리

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/member/model/dao/MemberLoginInfoRepository.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/member/model/dao/MemberLoginInfoRepository.java
@@ -6,6 +6,30 @@ import org.springframework.stereotype.Repository;
 
 import com.ssafy.team8alette.member.model.dto.MemberLoginInfo;
 
+/*
+// set
+    @PostMapping("")
+    public String setRedisKey(@RequestBody Map<String, String> req) {
+        ValueOperations<String, String> vop = redisTemplate.opsForValue();
+        try {
+            // Redis Set Key-value
+            System.out.println(req.get("key") + " // " + req.get("value"));
+            vop.set(req.get("key"), req.get("value"));
+            return "set message success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "set message fail";
+        }
+    }
+
+    // get
+    @GetMapping("/{key}")
+    public String getRedisKey(@PathVariable String key) {
+        System.out.println(key);
+        ValueOperations<String, String> vop = redisTemplate.opsForValue();
+        return vop.get(key);
+    }
+    */
 @Repository
 public class MemberLoginInfoRepository {
 	public void insertMemberLoginInfo(Long memberNumber, String refreshToken, boolean isSocialLogin) throws
