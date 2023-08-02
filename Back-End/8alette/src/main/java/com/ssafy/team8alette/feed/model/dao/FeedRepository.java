b3482417173f46be2662b46f6e286da70acabc7d
GyuhoChoi <s4248942@gmail.com>
2023-08-02 13:41:17 +0900

    Feat : 피드 좋아요 구현

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/feed/model/dao/FeedRepository.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/feed/model/dao/FeedRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.team8alette.feed.model.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,8 +16,10 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
 	//Long으로 구현해야함
 	Feed findFeedByFeedNumber(Long feedNumber);
 
-	List<Feed> findAllByOrderByFeedLikeCntDesc();
-	
-	//Long으로 삭제
-	// void deleteById(Long id);
+	List<Feed> findByFeedActiveOrderByFeedNumberDesc(boolean feedActive);
+
+	List<Feed> findByFeedActive(boolean feedActive, Sort sort);
+
+	boolean existsByFeedNumberAndFeedActive(Long feedNumber, boolean feedActive);
+
 }
