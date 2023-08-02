3be24034be9074bb60750f7bc0d66fb82dc6033f
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-02 09:17:33 +0900

    Feat : 신고기능 추가 2023-08-01 조성락

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/report/model/service/ReportService.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/report/model/service/ReportService.java
@@ -1,8 +1,12 @@
 package com.ssafy.team8alette.report.model.service;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import com.ssafy.team8alette.member.util.NullValueChecker;
 import com.ssafy.team8alette.report.model.dao.ReportRepository;
+import com.ssafy.team8alette.report.model.dto.Report;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,8 +15,19 @@ import lombok.RequiredArgsConstructor;
 public class ReportService {
 
 	private final ReportRepository reportRepository;
+	private final NullValueChecker nullValueChecker;
 
-	public void sendReport() {
+	public void sendReport(Map<String, String> param) {
+		Long reportReporter = Long.parseLong(param.get("reporter"));
+		Long reportSuspect = Long.parseLong(param.get("suspect"));
+		String reportReason = param.get(reportReporter);
+
+		Report report = new Report();
+		report.setReportReporter(reportReporter);
+		report.setReportSuspect(reportSuspect);
+		report.setReportReason(reportReason);
+
+		reportRepository.save(report);
 
 	}
 }
