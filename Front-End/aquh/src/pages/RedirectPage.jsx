71d9cbbd6c62fdfbf3f7553d5fe493e8294926de
Jiyoung Park <parkninni@icloud.com>
2023-08-03 17:34:16 +0900

    Fix : api 경로 수정

--- Front-End/aquh/src/pages/RedirectPage.jsx
+++ Front-End/aquh/src/pages/RedirectPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import https from "../utils/https";
-import { loginNaver } from "../api/api.auth_service";
+import { loginNaver } from "../utils/api/api.auth_service";
 
 export default function RedirectURI(props) {
   const navigate = useNavigate();
