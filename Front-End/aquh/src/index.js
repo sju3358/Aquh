a6c68084669281354a8e17d830e1b7549efb2020
Jiyoung Park <parkninni@icloud.com>
2023-08-03 17:40:02 +0900

    Fix : GlobalStyle 수정

--- Front-End/aquh/src/index.js
+++ Front-End/aquh/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./globalStyle";
 import {
   RecoilRoot,
   atom,
@@ -17,6 +18,7 @@ import {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
+    <GlobalStyle />
     <App />
   </RecoilRoot>
 );
