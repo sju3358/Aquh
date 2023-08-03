05de63287bae08a209c90d5890b1527cb30c8690
ninnistic <parkninni@icloud.com>
2023-08-04 01:04:26 +0900

    Style : Navbar 스타일링 적용

--- Front-End/aquh/src/components/ui/Nav.jsx
+++ Front-End/aquh/src/components/ui/Nav.jsx
@@ -5,11 +5,14 @@ import classes from './Nav.module.css';
 export default function Nav() {
   return (
     <nav className={classes.navBar}>
-      <Link to='/'> <img src="../../aquh-logo.png" alt="aquh-logo" /></Link>
+      <Link to='/'> <img src="../../aquh-logo.png" alt="aquh-logo" className={classes.logoPng} /></Link>
       <div className={classes.navItemContainer}>
-      <Link to='/feed' style={{ textDecoration : "none" }}><span className={classes.navItem}>피드</span></Link>
-      <Link to='/bubble' style={{ textDecoration : "none" }}><span className={classes.navItem}>버블</span></Link>
-      <Link to='/profile' style={{ textDecoration : "none" }}><span className={classes.navItem}>My</span></Link>
+        <div>
+          <Link to='/feed' style={{ textDecoration : "none" }}><span className={classes.navItem}>피드</span></Link>
+          <Link to='/bubble' style={{ textDecoration : "none" }}><span className={classes.navItem}>버블</span></Link>
+          <Link to='/profile' style={{ textDecoration : "none" }}><span className={classes.navItem}>My</span></Link>
+        </div>
+        <img src="../../avatar-image-circle.png" alt="search" className={classes.avatarPng} />  
       </div>     
     </nav>
   );
