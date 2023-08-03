00f0f1c2af10d258d8e12f0e1e25222f71dfe2dc
Jiyoung Park <parkninni@icloud.com>
2023-08-03 17:06:51 +0900

    Fix : bubblecard component 폰트이슈

--- Front-End/aquh/src/components/bubble/BubbleList.jsx
+++ Front-End/aquh/src/components/bubble/BubbleList.jsx
@@ -7,7 +7,7 @@ import classes from './BubbleList.module.css';
 //TODO : 이벤트 카드 정렬 및 레이아웃만 담당
 export default function BubbleList({ children }) {
   return (
-    <div className="{classes.bubbleList}">{ children }</div>
+    <div className={classes.bubbleList}>{ children }</div>
     /* TODO: add list styles */
   );
 }
