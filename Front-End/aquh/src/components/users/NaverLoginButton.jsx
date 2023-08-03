26b8413abd723256c4f3dadeb4ad6145f53004f7
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-03 23:46:52 +0900

    feat : 네이버로그인 리다이렉트 주소 변경 죄송합니다 이제 브랜치 떠서 할께요 여기까지만..ㅠ

--- Front-End/aquh/src/components/users/NaverLoginButton.jsx
+++ Front-End/aquh/src/components/users/NaverLoginButton.jsx
@@ -5,7 +5,7 @@ export default function NaverLoginButton({}) {
   const NAVER_CLIENT_ID = "0jXgPVDyLQu_ekRssB20";
   //TODO : 나중에 Redirect URI를 http://i9b108.p.ssafy.io:3000/ 로 바꾸기
   //TODO : redirect URI를 main에서 닉네임 설정 화면으로 연결하기
-  const REDIRECT_URI = "http://localhost:3000/redirect";
+  const REDIRECT_URI = "http://i9b108.p.ssafy.io:3000/redirect";
   const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI}`;
 
   const handleNaverLogin = () => {
