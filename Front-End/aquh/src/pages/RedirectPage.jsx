cd58342f0013b9cc91fa28b7b1207b725ee043ea
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-03 17:27:42 +0900

    Feat : Recoil 설정

--- Front-End/aquh/src/pages/RedirectPage.jsx
+++ Front-End/aquh/src/pages/RedirectPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import https from "../utils/https";
 import { loginNaver } from "../api/api.auth_service";
 
 export default function RedirectURI(props) {
@@ -10,8 +10,8 @@ export default function RedirectURI(props) {
     let code = new URL(window.location.href).searchParams.get("code");
     let state = new URL(window.location.href).searchParams.get("state");
     console.log(code, state);
-    axios
-      .post("http://i9b108.p.ssafy.io:8080/api/v1/member/auth/naver", {
+    https
+      .post("/api/v1/member/auth/naver", {
         code: code,
         state: state,
       })
