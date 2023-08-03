95fb2e69f80937be35626fded1d52fa250d05e3d
GyuhoChoi <s4248942@gmail.com>
2023-08-04 03:26:59 +0900

    Fix : 파일 경로 수정

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/follow/model/dto/Entity/Follow.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/follow/model/dto/Entity/Follow.java
@@ -1,45 +1,46 @@
 package com.ssafy.team8alette.follow.model.dto.Entity;
 
-import java.io.Serializable;
 import java.util.Date;
 
-import com.ssafy.team8alette.follow.model.dto.Key.FollowID;
 import com.ssafy.team8alette.member.model.dto.Member;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
 @Table(name = "follow")
-public class Follow implements Serializable {
+@ToString
+public class Follow {
 
-	@EmbeddedId
-	private FollowID followID;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "follow_number", nullable = false)
+	private Long followNumber;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "follower_number")
+	private Member followerMemberNumber;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "following_number")
+	private Member followingMemberNumber;
 
 	@Column(name = "create_dttm", nullable = false)
 	private Date createDate;
-
-	@MapsId("followerNumber")
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_number")
-	private Member followerMember;
-
-	@MapsId("followingNumber")
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_number")
-	private Member followingMember;
 
 	// @Id
 	// private Long followerMemberNumber;
