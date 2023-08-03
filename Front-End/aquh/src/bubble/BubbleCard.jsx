d177bc590a7a1052b1d03fa17e3a84e88d82c731
Jiyoung Park <parkninni@icloud.com>
2023-08-03 11:14:33 +0900

    Style : Button Component styling

--- Front-End/aquh/src/bubble/BubbleCard.jsx
+++ Front-End/aquh/src/bubble/BubbleCard.jsx
@@ -9,10 +9,10 @@ export default function BubbleCard({
   eventTitle = "Untitled",
   eventContent = "Some content...",
   imagePath,
-  onJoin = () => {},
-  label = ""
+  onJoin = () => {}
 }) {
 
+  // So just a few minor changes here
   return (
     <div className={classes.card}>
       {imagePath && <img className={classes.cardImgTop} src={imagePath} alt="Event" />}
@@ -27,7 +27,11 @@ export default function BubbleCard({
       <div className="card-footer">
         <p>Event Date</p>
       </div>
-      <Button onClick={onJoin} label={label} className="card-button"> Join </Button>
+      <Button onClick={onJoin} variant="regular" className="card-button">Join</Button>
+      { /* Ah see you wrote it with the label between the tags at first. That will work now! */}
+      {/* so is it working rn? Wait one question...why does the BubbleCard have a label prop? */}
+      {/* we dont need it? The button always says "Join" right? ahh right . okkee I will delete it then */}
+      {/* "regular" is the default but I am going to put it anyway so it's more clear */}
     </div>
     
   )
