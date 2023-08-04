89500fde618f89ca4958cacc38822d1a891dce96
Jiyoung Park <parkninni@icloud.com>
2023-08-04 14:11:11 +0900

    Style : loginPage 마우스 효과 수정 및 경로 수정

--- Front-End/aquh/src/App.js
+++ Front-End/aquh/src/App.js
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
     errorElement : <NotFound />,
     children : [
       { index: 'true', element : <MainPage /> },
-      { path: '/login', element : <LoginModal />},
+      { path: '/login', element : <LoginPage />},
       { path: '/auth', element : <AuthPage />},
       // TODO : SignupForm을 SignUpPage로 바꾸기. 
       { path: '/signup', element : <SignUpForm />},
