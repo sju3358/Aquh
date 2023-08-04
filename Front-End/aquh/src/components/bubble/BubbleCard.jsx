936bb2d88a8ba5339772c4a368ff50614f089bad
seol <seolhyehyeon@gmail.com>
2023-08-04 15:17:38 +0900

    Fix : 피드 작성2

--- Front-End/aquh/src/components/bubble/BubbleCard.jsx
+++ Front-End/aquh/src/components/bubble/BubbleCard.jsx
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import classes from './BubbleCard.module.css'
-import Capsule from '../ui/Capsule';
-import { bubbleList } from '../../utils/api/api.bubble_service';
-import Button from '../ui/Button';
-import { FaRegCalendarCheck } from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import classes from "./BubbleCard.module.css";
+import Capsule from "../ui/Capsule";
+import { bubbleList } from "../../utils/api/api.bubble_service";
+import Button from "../ui/Button";
+import { FaRegCalendarCheck } from "react-icons/fa";
 
 export default function BubbleCard({
   roomType = 0,
   eventTitle = "Untitled",
   eventDate = "Some Date..",
   imagePath,
-  onJoin = () => {}
+  onJoin = () => {},
 }) {
-
   // So just a few minor changes here
   return (
     <div className={classes.card}>
-      {imagePath && <img className={classes.cardImgTop} src={imagePath} alt="Event" />}
+      {imagePath && (
+        <img className={classes.cardImgTop} src={imagePath} alt='Event' />
+      )}
       {/* TODO: roomType */}
-        {/* <Capsule /> */}
-        <p className={classes.eventTitle}>{eventTitle}</p>
-        <p className={classes.eventDate}> <FaRegCalendarCheck /> {eventDate}</p>
-      <Button onClick={onJoin} variant="regular" className="card-button">참여하기</Button>
+      {/* <Capsule /> */}
+      <p className={classes.eventTitle}>{eventTitle}</p>
+      <p className={classes.eventDate}>
+        {" "}
+        <FaRegCalendarCheck /> {eventDate}
+      </p>
+      <Button onClick={onJoin} variant='regular' className='card-button'>
+        참여하기
+      </Button>
     </div>
-    
-  )
+  );
 }
