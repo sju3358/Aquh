e45d5c4b5d7d8ecc72c3ce27332c9d898ee0d6be
GyuhoChoi <s4248942@gmail.com>
2023-08-06 04:01:44 +0900

    Fix : 알람 기능 구현 완료

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/alarm/controller/AlarmController.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/alarm/controller/AlarmController.java
@@ -1,7 +1,9 @@
 package com.ssafy.team8alette.alarm.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +13,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.team8alette.alarm.method.AlarmListMapper;
+import com.ssafy.team8alette.alarm.model.dto.Alarm;
+import com.ssafy.team8alette.alarm.model.dto.AlarmListResponseDTO;
 import com.ssafy.team8alette.alarm.model.service.AlarmService;
 
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/api/v1/alarm")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/alarm")
 public class AlarmController {
 
 	private final AlarmService alarmService;
@@ -26,9 +31,14 @@ public class AlarmController {
 	public ResponseEntity<Map<String, Object>> alarmListRequest(@PathVariable Long memberNumber) {
 
 		Map<String, Object> responseData = new HashMap<>();
+		List<Alarm> alarmList = alarmService.getAlarmList(memberNumber);
+		List<AlarmListResponseDTO> dtoList = alarmList.stream()
+			.map(AlarmListMapper::convertToDTO)
+			.collect(Collectors.toList());
+
+		responseData.put("alarmList", dtoList);
 		responseData.put("message", "success");
 		responseData.put("status", 200);
-		responseData.put("data", alarmService.getAlarmList(memberNumber));
 
 		return new ResponseEntity<>(responseData, HttpStatus.OK);
 	}
