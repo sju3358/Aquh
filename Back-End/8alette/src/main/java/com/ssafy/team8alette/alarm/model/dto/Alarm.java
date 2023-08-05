b105a57be0b469b0a1d0ba0fb38aebd93a349b77
GyuhoChoi <s4248942@gmail.com>
2023-08-06 04:01:44 +0900

    Fix : 알람 기능 구현 완료

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/alarm/model/dto/Alarm.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/alarm/model/dto/Alarm.java
@@ -1,34 +1,55 @@
 package com.ssafy.team8alette.alarm.model.dto;
 
+import java.util.Date;
+
+import com.ssafy.team8alette.member.model.dto.Member;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@RequiredArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "alarm")
 public class Alarm {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "alarm_number")
+	@Column(name = "alarm_number", nullable = false)
 	private Long alarmNumber;
 
-	@Column(name = "member_number")
-	private Long memberNumber;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_number", nullable = false)
+	private Member memberNumber;
 
-	@Column(name = "alarm_text")
-	private String alarmText;
+	@Column(name = "alarm_type", nullable = false)
+	private String alarmType;
 
-	@Column(name = "alarm_state")
+	@Column(name = "alarm_reason")
+	private String alarmReason;
+
+	@Column(name = "alarm_state", nullable = false)
 	private int alarmState;
+
+	@Column(name = "read_dttm")
+	private Date readDateTime;
+
+	@Column(name = "delete_dttm")
+	private Date deleteDateTime;
+	// @Column(name = "alarm_text", nullable = false)
+	// private String alarmText;
 
 }
