832d407697c59cd90f31e66ef4baa6c5f74653c3
seol <seolhyehyeon@gmail.com>
2023-08-06 00:12:37 +0900

    Fix : 피드 글 생성 완료

--- Front-End/aquh/src/components/feed/FeedWrite.jsx
+++ Front-End/aquh/src/components/feed/FeedWrite.jsx
@@ -5,10 +5,8 @@ import classes from "./FeedWrite.module.css";
 function FeedWrite() {
   const [feedTitle, setFeedTitle] = useState("");
   const [feedContent, setFeedConTent] = useState("");
-  // const [fileName, setFileName] = useState("");
-  const [file, setFile] = useState(null); // 파일 객체를 저장하는 상태 추가
+  const [file, setFile] = useState(null);
 
-  // 유저가 작성하여 값 변경
   const onChangeFeedTitle = (e) => {
     const currentFeedTitle = e.target.value;
     setFeedTitle(currentFeedTitle);
@@ -18,22 +16,16 @@ function FeedWrite() {
     setFeedConTent(currentFeedContent);
   };
   const onChangeFeedFile = (e) => {
-    //   const currentFeedFile = e.target.files[0];
-    //   setFileName(currentFeedFile);
-    //   console.log("여기 파일임", currentFeedFile);
-    // };
     const currentFile = e.target.files[0];
-    setFile(currentFile); // 파일 객체를 상태에 저장
+    setFile(currentFile);
     console.log("여기 파일임", currentFile);
   };
 
-  //  버튼 클릭했을 때 axios 전송
   const onClinkWriteBtn = () => {
     const memberNumber = localStorage.getItem("member_number");
     console.log("멤버넘버", memberNumber);
-    // TODO : atom에서 가져오기
+
     if (feedTitle && feedContent) {
-      //제목이랑 내용이 다 채워져 있을 경우
       const formData = new FormData();
       const jsonData = {
         member: {
@@ -43,28 +35,21 @@ function FeedWrite() {
         content: feedContent,
       };
 
-      formData.append("feed", JSON.stringify(jsonData));
-      // if (fileName) {
-      //   formData.append("file", fileName);
-      // } else {
-      //   formData.append("file", new Blob());
-      // }
+      // Append the JSON data under a different key
+      formData.append(
+        "feed",
+        new Blob([JSON.stringify(jsonData)], { type: "application/json" })
+      );
 
       if (file) {
         formData.append("file", file);
       } else {
-        formData.append("file", new Blob());
+        formData.append("file", new Blob(), "empty");
       }
-      console.log("파일 : ", file.name);
-      // for (let key of formData.keys()) {
-      //   console.log("파일 : ", formData.get(key.name));
-      //   console.log(key, ":", formData.get(key));
-      // }
 
       axios
         .post("http://localhost:8080/api/v1/feed", formData, {
           headers: {
-            "Content-Type": "multipart/form-data", // 중요: 'multipart/form-data'로 설정해야 합니다.
             "AUTH-TOKEN": localStorage.getItem("access_token"),
           },
         })
@@ -80,10 +65,8 @@ function FeedWrite() {
       alert("글 내용을 작성해주세요");
     }
   };
-
   return (
     <div className={classes.feedWriteCard}>
-      {/* 피드 제목 */}
       <div className={classes.feedTitle}>
         <input
           type='text'
@@ -92,7 +75,6 @@ function FeedWrite() {
           placeholder='제목을 입력하세요'
         />
       </div>
-      {/* 피드 내용 */}
       <div className={classes.feedContent}>
         <textarea
           cols='30'
@@ -101,7 +83,6 @@ function FeedWrite() {
           onChange={onChangeFeedContent}
           placeholder='내용을 입력하세요'></textarea>
       </div>
-      {/* 피드 파일 */}
       <div className={classes.feedFile}>
         <input onChange={onChangeFeedFile} accept='image/*' type='file' />
       </div>
@@ -109,4 +90,5 @@ function FeedWrite() {
     </div>
   );
 }
+
 export default FeedWrite;
