2210b9bf2a6e58983650089bc2912dabe3b253e7
ninnistic <parkninni@icloud.com>
2023-08-06 23:07:20 +0900

    Add : Navbar에 검색추가

--- Front-End/aquh/src/components/ui/Nav.jsx
+++ Front-End/aquh/src/components/ui/Nav.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './Nav.module.css';
+import SearchInput from '../search/SearchInput';
 
 export default function Nav() {
   return (
     <nav className={classes.navBar}>
       <Link to='/'> <img src="../../aquh-logo.png" alt="aquh-logo" className={classes.logoPng} /></Link>
       <div className={classes.navItemContainer}>
-        <div>
+        <div className={classes.menuItems}>
           <Link to='/feed' style={{ textDecoration : "none" }}><span className={classes.navItem}>피드</span></Link>
           <Link to='/bubble' style={{ textDecoration : "none" }}><span className={classes.navItem}>버블</span></Link>
           <Link to='/profile' style={{ textDecoration : "none" }}><span className={classes.navItem}>My</span></Link>
         </div>
+        <div className={classes.searchContainer}>
+        <SearchInput />
         <img src="../../avatar-image-circle.png" alt="search" className={classes.avatarPng} />  
+        </div>
       </div>     
     </nav>
   );
