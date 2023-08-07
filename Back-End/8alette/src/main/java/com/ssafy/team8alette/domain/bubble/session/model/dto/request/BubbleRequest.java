6b5ee88f53638c565c917e844196fa6672fdbfbd
tjdfkr011 <tjdfkr011@naver.com>
2023-08-07 17:53:12 +0900

    Feat : 버블방 Session 생성 서비스 개발 (임시 저장)

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/session/model/dto/request/BubbleRequest.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/session/model/dto/request/BubbleRequest.java
@@ -1,4 +1,26 @@
 package com.ssafy.team8alette.domain.bubble.session.model.dto.request;
 
+import java.util.Date;
+
+import com.ssafy.team8alette.domain.bubble.session.model.dto.entity.CategoryEntity;
+import com.ssafy.team8alette.domain.member.auth.model.dto.Member;
+
 public class BubbleRequest {
+
+	private Member hostMemberNumber;
+
+	private CategoryEntity categoryEntity;
+
+	private boolean bubbleType;
+
+	private String bubbleTitle;
+
+	private String bubbleContent;
+
+	private String bubbleThumbnail;
+
+	private Date planOpenDate;
+
+	private Date planCloseDate;
+
 }
