9d0040716b8f2beef89038752b11b050995ce77c
GyuhoChoi <s4248942@gmail.com>
2023-08-07 17:41:14 +0900

    Feat : 회원가입시 기록 필드 자동생성

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/symbol/model/service/SymbolService.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/symbol/model/service/SymbolService.java
@@ -1,4 +1,19 @@
 package com.ssafy.team8alette.domain.symbol.model.service;
 
+import org.springframework.stereotype.Service;
+
+import com.ssafy.team8alette.domain.symbol.model.dao.SymbolRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class SymbolService {
+
+	private final SymbolRepository symbolRepository;
+
+	// public List<Symbol> getAllSymbols(){
+	// 	return
+	// }
+
 }
