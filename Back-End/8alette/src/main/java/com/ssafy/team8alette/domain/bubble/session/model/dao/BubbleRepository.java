03b974238707658dccb4f3c9f7500e24c61d7c66
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-07 16:29:51 +0900

    Revert "Feat : 버블방 Session 생성 서비스 개발 (임시 저장)"
    This reverts commit 923450a342ec427831b42ac8bd6120638646ea78.

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/session/model/dao/BubbleRepository.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/session/model/dao/BubbleRepository.java
@@ -1,8 +1,4 @@
 package com.ssafy.team8alette.domain.bubble.session.model.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.team8alette.domain.bubble.session.model.dto.entity.BubbleEntity;
-
-public interface BubbleRepository extends JpaRepository<BubbleEntity, Long> {
+public class BubbleRepository {
 }
--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/session/model/dto/request/BubbleRequest.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.team8alette.domain.bubble.session.model.dto.request;
-
-public class BubbleRequest {
-}
