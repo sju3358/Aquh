e9726efe94ede17f83be3471706f5929a7574b8f
ninnistic <parkninni@icloud.com>
2023-08-08 23:10:56 +0900

    Chore : 필요없는 구문 삭제 및 dummy jsx 생성

--- Front-End/aquh/src/pages/AuthPage.jsx
+++ Front-End/aquh/src/pages/AuthPage.jsx
@@ -30,40 +30,6 @@ function AuthPage() {
         <SymbolList />
       </div>
     </main>
-
-    // <div className="authPage">
-    //   <h3 className="authPageText">개인정보(수정 전) 모달 창 입니다.</h3>
-    //   <div className="userCharIdLvSym">
-    //     <div className="userIcon">
-    //       <img className="userChar" src="" alt="User캐릭터" />
-    //       <div>
-    //         <h4>"유저 아이디"</h4>
-    //         <h6>"유저 레벨"</h6>
-    //       </div>
-    //     </div>
-    //     <div className="userSymbol">
-    //       <h4>나의 심볼</h4>
-    //       <div>[심볼 목록]</div>
-    //     </div>
-    //   </div>
-    //   <div className="userInfo">
-    //     <div className="authPageUsername">
-    //       <h4>닉네임 : </h4>
-    //       <p>{memberNickname}</p>
-    //     </div>
-    //     <div className="authPageEmail">
-    //       <h4>이메일 : </h4>
-    //       <p>{memberEmail}</p>
-    //     </div>
-    //     <div className="authPagePassword">
-    //       <button>비밀번호 변경</button>
-    //     </div>
-    //     <div>
-    //       <button>회원탈퇴</button>
-    //     </div>
-    //   </div>
-    //   <button>수정하기</button>
-    // </div>
   );
 }
 
