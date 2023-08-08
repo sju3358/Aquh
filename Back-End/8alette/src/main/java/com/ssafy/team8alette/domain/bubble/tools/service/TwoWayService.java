a8a71c5c88d96e99b242291f39f9675324bcb782
Jade_Good <kangsm423@gmail.com>
2023-08-09 00:17:17 +0900

    Add : two-way Controller - answer 생성/수정

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/service/TwoWayService.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/service/TwoWayService.java
@@ -19,76 +19,88 @@ import com.ssafy.team8alette.domain.member.auth.model.dao.MemberRepository;
 import com.ssafy.team8alette.domain.member.auth.model.dto.Member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class TwoWayService {
-	private final TwoWayQuestionRepository twoWayQuestionRepository;
-	private final TwoWayAnswerRepository twoWayAnswerRepository;
-	private final BubbleRepository bubbleRepository;
-	private final MemberRepository memberRepository;
+    private final TwoWayQuestionRepository twoWayQuestionRepository;
+    private final TwoWayAnswerRepository twoWayAnswerRepository;
+    private final BubbleRepository bubbleRepository;
+    private final MemberRepository memberRepository;
 
-	// 양자택일 질문 전체 조회
-	public List<TwoWayQuestionResponseDTO> getTwoWayQuestions(Long bubbleNumber, Long memberNumber) {
-		BubbleEntity bubbleEntity = bubbleRepository.findById(bubbleNumber).orElseThrow();
-		List<TwoWayQuestionEntity> questions = twoWayQuestionRepository.findAllByBubbleEntity(bubbleEntity);
-		List<TwoWayQuestionResponseDTO> response = new ArrayList<>();
-		int pick_member_cnt = questions.size();
+    // 양자택일 질문 전체 조회
+    public List<TwoWayQuestionResponseDTO> getTwoWayQuestions(Long bubbleNumber, Long memberNumber) {
+        BubbleEntity bubbleEntity = bubbleRepository.findById(bubbleNumber).orElseThrow();
+        List<TwoWayQuestionEntity> questions = twoWayQuestionRepository.findAllByBubbleEntity(bubbleEntity);
+        List<TwoWayQuestionResponseDTO> response = new ArrayList<>();
+        int pick_member_cnt = questions.size();
 
-		for (TwoWayQuestionEntity question : questions) {
-			List<TwoWayAnswerEntity> answers = twoWayAnswerRepository.findAllByTwoWayQuestionEntity(question);
+        for (TwoWayQuestionEntity question : questions) {
+            List<TwoWayAnswerEntity> answers = twoWayAnswerRepository.findAllByTwoWayQuestionEntity(question);
 
-			int left_cnt = 0;
-			int is_pick = 0;
+            int left_cnt = 0;
+            int is_pick = 0;
 
-			for (TwoWayAnswerEntity answer : answers) {
-				if (answer.getMember().getMemberNumber() == memberNumber)
-					is_pick = answer.isPickRight() ? 2 : 1;
-				if (!answer.isPickRight())
-					left_cnt++;
-			}
-			TwoWayQuestionResponseDTO dto = new TwoWayQuestionResponseDTO();
-			dto.setTwo_way_question_number(question.getTwoWayQuestionNumber());
-			dto.setLeft_context(question.getLeftContext());
-			dto.setRight_context(question.getRightContext());
-			dto.setPick_member_cnt(answers.size());
-			dto.setLeft_cnt(left_cnt);
-			dto.setMy_pick(is_pick);
+            for (TwoWayAnswerEntity answer : answers) {
+                if (answer.getMember().getMemberNumber() == memberNumber) {
+                    is_pick = answer.isPickRight() ? 2 : 1;
+                }
+                if (!answer.isPickRight()) {
+                    left_cnt++;
+                }
+            }
+            TwoWayQuestionResponseDTO dto = new TwoWayQuestionResponseDTO();
+            dto.setTwo_way_question_number(question.getTwoWayQuestionNumber());
+            dto.setLeft_context(question.getLeftContext());
+            dto.setRight_context(question.getRightContext());
+            dto.setPick_member_cnt(answers.size());
+            dto.setLeft_cnt(left_cnt);
+            dto.setMy_pick(is_pick);
 
-			response.add(dto);
-		}
+            response.add(dto);
+        }
 
-		return response;
-	}
+        return response;
+    }
 
-	// 양자택일 질문 등록
-	public void registTwoWayQuestion(TwoWayQuestionRequestDTO twoWayQuestionRequestDTO) {
-		BubbleEntity bubbleEntity = bubbleRepository.findById(twoWayQuestionRequestDTO.getBubble_number())
-			.orElseThrow();
-		TwoWayQuestionEntity twoWayQuestionEntity = new TwoWayQuestionEntity();
-		twoWayQuestionEntity.setBubbleEntity(bubbleEntity);
-		twoWayQuestionEntity.setLeftContext(twoWayQuestionRequestDTO.getLeft_context());
-		twoWayQuestionEntity.setRightContext(twoWayQuestionRequestDTO.getRight_context());
-		twoWayQuestionRepository.save(twoWayQuestionEntity);
-	}
+    // 양자택일 질문 등록
+    public void registTwoWayQuestion(TwoWayQuestionRequestDTO twoWayQuestionRequestDTO) {
+        BubbleEntity bubbleEntity = bubbleRepository.findById(twoWayQuestionRequestDTO.getBubble_number())
+                .orElseThrow();
+        TwoWayQuestionEntity twoWayQuestionEntity = new TwoWayQuestionEntity();
+        twoWayQuestionEntity.setBubbleEntity(bubbleEntity);
+        twoWayQuestionEntity.setLeftContext(twoWayQuestionRequestDTO.getLeft_context());
+        twoWayQuestionEntity.setRightContext(twoWayQuestionRequestDTO.getRight_context());
+        twoWayQuestionRepository.save(twoWayQuestionEntity);
+    }
 
-	// 양자택일 질문 삭제
-	public void deleteTwoWayQuestion(Long twoWayQuestionNumber) {
-		twoWayQuestionRepository.deleteById(twoWayQuestionNumber);
-	}
+    // 양자택일 질문 삭제
+    public void deleteTwoWayQuestion(Long twoWayQuestionNumber) {
+        twoWayQuestionRepository.deleteById(twoWayQuestionNumber);
+    }
 
-	// 양자택일 답변 등록
-	public void registTwoWayAnswer(TwoWayQuestionAnswerDTO twoWayQuestionAnswerDTO) {
-		TwoWayQuestionEntity twoWayQuestionEntity = twoWayQuestionRepository.findById(
-			twoWayQuestionAnswerDTO.getTwo_way_question_number()).orElseThrow();
-		Member member = memberRepository.findById(twoWayQuestionAnswerDTO.getMember_number()).orElseThrow();
-		TwoWayAnswerEntity twoWayAnswerEntity = new TwoWayAnswerEntity();
-		twoWayAnswerEntity.setTwoWayQuestionEntity(twoWayQuestionEntity);
-		twoWayAnswerEntity.setMember(member);
-		twoWayAnswerEntity.setPickRight(twoWayQuestionAnswerDTO.is_pick_right());
+    // 양자택일 답변 등록/수정
+    @Transactional
+    public void registTwoWayAnswer(TwoWayQuestionAnswerDTO twoWayQuestionAnswerDTO) {
+        TwoWayQuestionEntity twoWayQuestionEntity = twoWayQuestionRepository.findById(
+                twoWayQuestionAnswerDTO.getTwo_way_question_number()).orElseThrow();
+        Member member = memberRepository.findById(twoWayQuestionAnswerDTO.getMember_number()).orElseThrow();
 
-		System.out.println(twoWayAnswerEntity);
+        TwoWayAnswerID twoWayAnswerID = new TwoWayAnswerID();
+        twoWayAnswerID.setTwoWayQuestionNumber(twoWayQuestionAnswerDTO.getTwo_way_question_number());
+        twoWayAnswerID.setMemberNumber(twoWayQuestionAnswerDTO.getMember_number());
 
-		twoWayAnswerRepository.save(twoWayAnswerEntity);
-	}
+        if(twoWayAnswerRepository.existsByTwoWayAnswerID(twoWayAnswerID)) {
+            twoWayAnswerRepository.deleteByTwoWayAnswerID(twoWayAnswerID);
+        }
+
+        TwoWayAnswerEntity twoWayAnswerEntity = new TwoWayAnswerEntity();
+        twoWayAnswerEntity.setTwoWayQuestionEntity(twoWayQuestionEntity);
+        twoWayAnswerEntity.setMember(member);
+        twoWayAnswerEntity.setPickRight(twoWayQuestionAnswerDTO.is_pick_right());
+        twoWayAnswerEntity.setTwoWayAnswerID(twoWayAnswerID);
+
+        twoWayAnswerRepository.save(twoWayAnswerEntity);
+    }
 }
