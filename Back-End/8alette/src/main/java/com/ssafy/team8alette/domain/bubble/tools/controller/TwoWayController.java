3e0e7af1c4faf5ff7ab42ac5ab33ee7b689a97e5
Soomin_Kang <kangsm423@gmail.com>
2023-08-08 12:42:30 +0900

    Fix : two-way Controller off

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/controller/TwoWayController.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/controller/TwoWayController.java
@@ -1,17 +1,8 @@
 package com.ssafy.team8alette.domain.bubble.tools.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.team8alette.domain.bubble.session.model.service.BubbleService;
 import com.ssafy.team8alette.domain.bubble.tools.service.TwoWayService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,23 +12,25 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/twoway")
 public class TwoWayController {
 	private final TwoWayService twoWayService;
-	private final BubbleService bubbleService;
+	// private final BubbleService bubbleService;
 
-	@GetMapping("/question/{bubble_number")
-	public ResponseEntity<Map<String, Object>> findALlTwoWayQuestions(@PathVariable Long bubble_number) {
-		Map<String, Object> responseData = new HashMap<>();
-		responseData.put("message", "success");
-		responseData.put("status", 200);
-		return new ResponseEntity<>(responseData, HttpStatus.OK);
-	}
-
-	@PutMapping("/{feed_number}")
-	public ResponseEntity<?> deleteFeed(@PathVariable Long feed_number) {
-
-		feedService.deleteFeed(feed_number);
-		Map<String, Object> responseData = new HashMap<>();
-		responseData.put("message", "success");
-		responseData.put("status", 200);
-		return new ResponseEntity<>(responseData, HttpStatus.OK);
-	}
+	// @GetMapping("/question/{bubble_number")
+	// public ResponseEntity<Map<String, Object>> findALlTwoWayQuestions(@PathVariable Long bubble_number) {
+	// 	Map<String, Object> responseData = new HashMap<>();
+	// 	Map<String, Object> data = new HashMap<>();
+	//
+	// 	responseData.put("message", "success");
+	// 	responseData.put("status", 200);
+	// 	return new ResponseEntity<>(responseData, HttpStatus.OK);
+	// }
+	//
+	// @PutMapping("/{feed_number}")
+	// public ResponseEntity<?> deleteFeed(@PathVariable Long feed_number) {
+	//
+	// 	feedService.deleteFeed(feed_number);
+	// 	Map<String, Object> responseData = new HashMap<>();
+	// 	responseData.put("message", "success");
+	// 	responseData.put("status", 200);
+	// 	return new ResponseEntity<>(responseData, HttpStatus.OK);
+	// }
 }
