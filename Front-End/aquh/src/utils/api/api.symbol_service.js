cb99c98b008137595fb89253b57b6cb12ea7f17e
Jiyoung Park <parkninni@icloud.com>
2023-08-09 16:40:15 +0900

    Fix : api 요청시 axios interceptor로 변경

--- Front-End/aquh/src/utils/api/api.symbol_service.js
+++ Front-End/aquh/src/utils/api/api.symbol_service.js
@@ -1,16 +1,17 @@
-import axios from 'axios';
+// import axios from 'axios';
+import https from '../https';
 
-const accessToken = localStorage.getItem('access_token');
+// const accessToken = localStorage.getItem('access_token');
 
-const api = axios.create({
-    baseURL: 'https://i9b108.p.ssafy.io/api/v1/symbol',
-    headers: { 'Content-Type' : 'application/json'
-        ,'AUTH-TOKEN': accessToken }
-});
+// const api = axios.create({
+//     baseURL: 'https://i9b108.p.ssafy.io/api/v1/symbol',
+//     headers: { 
+//         'Content-Type' : 'application/json' }
+// });
 
 
 export const symbolList = (member_number) => 
-    api.get(`list/${member_number}`);
+    https.get(`api/v1/symbol/list/${member_number}`);
 
 export const mySymbolList = (member_number) => 
-    api.get(`grant/${member_number}`);
+    https.get(`api/v1/symbol/grant/${member_number}`);
