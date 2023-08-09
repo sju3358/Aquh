2f221bcb2fbd6fb69ba63e8aed805c073a6a3987
seol <seolhyehyeon@gmail.com>
2023-08-09 15:59:21 +0900

    Feat : 최초 로그인시 닉네임 등록

--- Front-End/aquh/src/App.js
+++ Front-End/aquh/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './pages/Root';
-import MainPage from './pages/MainPage';
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import MainPage from "./pages/MainPage";
 // import LoginModal from './pages/LoginModal';
-import NotFound from './components/ui/NotFound';
-import AuthPage from './pages/AuthPage';
-import SignUpForm from './components/users/SignUpForm';
-import BubblePage from './pages/BubblePage';  
-import RedirectPage from './pages/RedirectPage'
-import LoginPage from './pages/LoginPage';
-import FeedPage from './pages/FeedPage';
-import VideoPage from './pages/VideoPage';
+import NotFound from "./components/ui/NotFound";
+import AuthPage from "./pages/AuthPage";
+import SignUpForm from "./components/users/SignUpForm";
+import BubblePage from "./pages/BubblePage";
+import RedirectPage from "./pages/RedirectPage";
+import LoginPage from "./pages/LoginPage";
+import FeedPage from "./pages/FeedPage";
+import VideoPage from "./pages/VideoPage";
+import NicknamePage from "./pages/NicknamePage";
 
 const router = createBrowserRouter([
   // { path: '/login',
@@ -19,32 +20,29 @@ const router = createBrowserRouter([
   //   errorElement : <NotFound />,
   // },
 
-  
   {
-    path: '/',
-    element : <Root />,
-    errorElement : <NotFound />,
-    children : [
-      { index: 'true', element : <MainPage /> },
-      { path: '/login', element : <LoginPage />},
-      { path: '/auth/:userId', element : <AuthPage />},
-      // TODO : SignupForm을 SignUpPage로 바꾸기. 
-      { path: '/feed', element : <FeedPage />},
-      { path: '/bubble', element : <BubblePage />},
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: "true", element: <MainPage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/auth/:userId", element: <AuthPage /> },
+      // TODO : SignupForm을 SignUpPage로 바꾸기.
+      { path: "/feed", element: <FeedPage /> },
+      { path: "/bubble", element: <BubblePage /> },
       // 0807 김재원 수정
-      { path: '/video', element : <VideoPage />},
-      
-    ]
+      { path: "/video", element: <VideoPage /> },
+      { path: "/nickname", element: <NicknamePage /> },
+    ],
   },
   {
-    path:'/redirect',
-    element : <RedirectPage/>,
-    errorElement : <NotFound />,
-  }
-
+    path: "/redirect",
+    element: <RedirectPage />,
+    errorElement: <NotFound />,
+  },
 ]);
 
 export default function App() {
   return <RouterProvider router={router} />;
 }
-
