70bcf0a9bc570103c3b62d77b9a384f78c2bd70a
Jiyoung Park <parkninni@icloud.com>
2023-08-09 13:30:32 +0900

    Chore : baseURL 주소 도메인으로 변경

--- Front-End/aquh/src/utils/api/api.auth_service.js
+++ Front-End/aquh/src/utils/api/api.auth_service.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:8080/api/v1/member',
+    baseURL: 'https://i9b108.p.ssafy.io/api/v1/member',
     headers: { 'Content-Type': 'application/json' }
 });
 export default api;
 
-export const login = ({ email, password }) =>
-    api.post('auth', { member_email: email, member_password: password });
+// export const login = ({ email, password }) =>
+//     api.post('auth', { member_email: email, member_password: password });
 
-export const loginGoogle = ({ code }) =>
-    api.post('auth/google', { code });
+// export const loginGoogle = ({ code }) =>
+//     api.post('auth/google', { code });
 
-export const loginNaver = ({ code, state }) =>
-    api.post('auth/naver', { code, state });
+// export const loginNaver = ({ code, state }) =>
+//     api.post('auth/naver', { code, state });
 
 // TODO: get documentation for this and adjust accordingly.
 export const refreshToken = () =>
