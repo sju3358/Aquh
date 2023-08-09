45a60cb407b452af9b7a4c02a5135f437543858b
Kanaloa21 <kjwkjw1104@gmail.com>
2023-08-09 16:09:53 +0900

    Fix: feedpage.jsx https 적용

--- Front-End/aquh/src/pages/FeedPage.jsx
+++ Front-End/aquh/src/pages/FeedPage.jsx
@@ -23,7 +23,7 @@ function FeedPage() {
   async function axiosNew() {
     await axios({
       method: "GET",
-      url: "http://i9b108.p.ssafy.io:8080/api/v1/feed/list",
+      url: "https://i9b108.p.ssafy.io/api/v1/feed/list",
       headers: {
         "AUTH-TOKEN": localStorage.getItem("access_token"),
         // TODO : recoil atom에서 받아오는걸로 추후 수정해야함
