3e6a1fef34a3fc6a90a495bb642cc08edd3ee2ef
Soomin_Kang <kangsm423@gmail.com>
2023-08-09 16:18:58 +0900

    Fix : todo Repository del import

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/model/dao/TodoRepository.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/model/dao/TodoRepository.java
@@ -1,13 +1,11 @@
 package com.ssafy.team8alette.domain.bubble.tools.model.dao;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.team8alette.domain.bubble.session.model.dto.entity.BubbleEntity;
-import com.ssafy.team8alette.domain.bubble.session.model.dto.entity.CategoryEntity;
 import com.ssafy.team8alette.domain.bubble.tools.model.dto.entity.TodoEntity;
 
 @Repository
