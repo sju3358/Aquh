60ab2c608aee3c374ce570d29d798d8b7e357c60
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-09 09:50:07 +0900

    Feat : 네이버 로그인 버튼 클라이언트 계정 수정 및 포트 삭제

--- Front-End/aquh/src/components/users/NaverLoginButton.jsx
+++ Front-End/aquh/src/components/users/NaverLoginButton.jsx
@@ -3,10 +3,10 @@ import classes from "./NaverLoginButton.module.css";
 
 export default function NaverLoginButton({}) {
   // TODO : .env 파일에 REACT_APP_NAVER_CLIENT_ID에 저장하기.
-  const NAVER_CLIENT_ID = "0jXgPVDyLQu_ekRssB20";
+  const NAVER_CLIENT_ID = "sOLMKlOcIu3pqoFbtMey";
   //TODO : 나중에 Redirect URI를 http://i9b108.p.ssafy.io:3000/ 로 바꾸기
   //TODO : redirect URI를 main에서 닉네임 설정 화면으로 연결하기
-  const REDIRECT_URI_SERVER = "https://i9b108.p.ssafy.io:3000/redirect";
+  const REDIRECT_URI_SERVER = "https://i9b108.p.ssafy.io/redirect";
   const REDIRECT_URI_LOCAL = "https://localhost:3000/redirect";
   const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI_SERVER}`;
 
--- Front-End/aquh/src/utils/https.js
+++ Front-End/aquh/src/utils/https.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let instance = axios.create({
-  baseURL: "https://i9b108.p.ssafy.io:8080",
+  baseURL: "https://i9b108.p.ssafy.io",
   headers: {
     "Content-Type": "application/json",
   },
