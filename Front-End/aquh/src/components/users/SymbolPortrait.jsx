5b50db18b02816c930ed09d47f6425f3b8713494
Jiyoung Park <parkninni@icloud.com>
2023-08-10 17:20:21 +0900

    Style : 획득하지 않은 심볼 흑백 임시저장

--- Front-End/aquh/src/components/users/SymbolPortrait.jsx
+++ Front-End/aquh/src/components/users/SymbolPortrait.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import classes from './SymbolPortrait.module.css';
 
-export default function SymbolPortrait({symbolImgName = "https://aquh.s3.ap-northeast-2.amazonaws.com/symbol/bb5.png", symbolName="베스트 프렌드"}) {
+export default function SymbolPortrait({symbolImgName = "https://aquh.s3.ap-northeast-2.amazonaws.com/symbol/bb5.png", symbolName="베스트 프렌드", isActive = false}) {
 
   const handleClick = () => {
     console.log("클릭됨")
   }
   return (
     <section className={classes.symbolCard}>
-    <div className={classes.imgBackground}>
+    <div className={`classes.imgBackground${!isActive ? 'Invalid' : '' }`}>
       <img src={symbolImgName} alt="symbolImgName" onClick={handleClick} className={classes.symbolImage}/>
     </div>
     </section>
