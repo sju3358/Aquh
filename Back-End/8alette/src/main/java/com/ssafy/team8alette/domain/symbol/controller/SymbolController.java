9361ae7942e154b515218d56655f61d19aaff3d9
GyuhoChoi <s4248942@gmail.com>
2023-08-11 01:58:25 +0900

    Feat : 마이페이지 심볼 조회 기능 구현

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/symbol/controller/SymbolController.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/symbol/controller/SymbolController.java
@@ -1,6 +1,5 @@
 package com.ssafy.team8alette.domain.symbol.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.team8alette.domain.symbol.model.dto.grant.response.GrantResponseDTO;
-import com.ssafy.team8alette.domain.symbol.model.dto.grant.response.SymbolListResponseDTO;
-import com.ssafy.team8alette.domain.symbol.model.dto.symbol.Symbol;
 import com.ssafy.team8alette.domain.symbol.model.service.SymbolGrantService;
 import com.ssafy.team8alette.domain.symbol.model.service.SymbolService;
 import com.ssafy.team8alette.global.annotation.LoginRequired;
@@ -33,35 +30,12 @@ public class SymbolController {
 	@GetMapping("/list/{memberNumber}")
 	public ResponseEntity<Map<String, Object>> getAllSymbol(@PathVariable Long memberNumber) {
 
-		List<Symbol> list = symbolService.getAllSymbols();
-
-		List<GrantResponseDTO> grantResponseDTOList = grantService.getGrantList(memberNumber);
-
-		List<Long> activeSymbolNumbers = new ArrayList<>();
-
-		for (GrantResponseDTO dto : grantResponseDTOList) {
-			if (dto.isSymbolActive()) {
-				activeSymbolNumbers.add(dto.getSymbolNumber());
-			}
-		}
-
-		List<SymbolListResponseDTO> dtoList = new ArrayList<>();
-		for (Symbol symbol : list) {
-			boolean isSymbolActive = activeSymbolNumbers.contains(symbol.getSymbolNumber());
-			SymbolListResponseDTO responseDTO = new SymbolListResponseDTO(
-				symbol.getSymbolNumber(),
-				symbol.getSymbolName(),
-				"https://aquh.s3.ap-northeast-2.amazonaws.com/symbol/" + symbol.getSymbolImgName(),
-				symbol.getSymbolCode(),
-				symbol.getSymbolConditionCnt(),
-				isSymbolActive);
-			dtoList.add(responseDTO);
-
-			//수정
-		}
+		List<GrantResponseDTO> requiredSymbolList = symbolService.getRequiredSymbolList(memberNumber);
+		List<GrantResponseDTO> remainList = symbolService.getRemainList(memberNumber);
 
 		Map<String, Object> responseData = new HashMap<>();
-		responseData.put("symbolList", dtoList);
+		responseData.put("requiredSymbolList", requiredSymbolList);
+		responseData.put("remainSymbolList", remainList);
 		responseData.put("message", "success");
 
 		return new ResponseEntity<>(responseData, HttpStatus.OK);
