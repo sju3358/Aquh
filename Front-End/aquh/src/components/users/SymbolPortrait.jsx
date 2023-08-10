514dbd0687d938b319115e45efda2026430f236f
Jiyoung Park <parkninni@icloud.com>
2023-08-10 17:37:48 +0900

    Style : 심볼 스타일 흑백 임시 적용

--- Front-End/aquh/src/components/users/SymbolPortrait.jsx
+++ Front-End/aquh/src/components/users/SymbolPortrait.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import classes from './SymbolPortrait.module.css';
 
-export default function SymbolPortrait({symbolImgName = "https://aquh.s3.ap-northeast-2.amazonaws.com/symbol/bb5.png", symbolName="베스트 프렌드", isActive = false}) {
+export default function SymbolPortrait({symbolImgName = "https://aquh.s3.ap-northeast-2.amazonaws.com/symbol/bb5.png", symbolName="베스트 프렌드", isActive}) {
 
   const handleClick = () => {
     console.log("클릭됨")
   }
   return (
     <section className={classes.symbolCard}>
-    <div className={`classes.imgBackground${!isActive ? 'Invalid' : '' }`}>
-      <img src={symbolImgName} alt="symbolImgName" onClick={handleClick} className={classes.symbolImage}/>
+    <div className={classes.imgBackground}>
+      {/* TODO: isActive를 획득여부로 교체해야함. */}
+      <img src={symbolImgName} alt="symbolImgName" onClick={handleClick} className={`${ !isActive ? classes.symbolImage : classes.symbolImageInvalid}`}/>
     </div>
     </section>
   );
