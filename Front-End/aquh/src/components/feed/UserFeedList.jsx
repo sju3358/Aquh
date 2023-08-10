6fcbb4e62a747776f149ab1533d83fe8eee95433
Jiyoung Park <parkninni@icloud.com>
2023-08-10 15:11:20 +0900

    Add : 유저 피드 조회 카드 생성

---
+++ Front-End/aquh/src/components/feed/UserFeedList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { memberNumberState } from '../../store/loginUserState';
+import { fetchUserFeedList } from '../../utils/api/api.feed_service';
+import UserFeedCard from './UserFeedCard';
+
+export default function UserFeedList() {
+
+  
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
