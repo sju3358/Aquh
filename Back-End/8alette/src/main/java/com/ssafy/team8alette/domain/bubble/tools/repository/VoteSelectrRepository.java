89d53a178e8bd0fcbb3c34367033b5bd5073c874
tjdfkr011 <tjdfkr011@naver.com>
2023-08-10 14:47:15 +0900

    FIX : 디렉토리 오류 수정

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/repository/VoteSelectrRepository.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/repository/VoteSelectrRepository.java
@@ -1,11 +1,11 @@
-package com.ssafy.team8alette.domain.bubble.tools.model.dao;
+package com.ssafy.team8alette.domain.bubble.tools.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ssafy.team8alette.domain.bubble.tools.model.dto.entity.VoteQuestionEntity;
-import com.ssafy.team8alette.domain.bubble.tools.model.dto.entity.VoteSelectEntity;
 import com.ssafy.team8alette.domain.bubble.tools.model.dto.key.VoteID;
+import com.ssafy.team8alette.domain.bubble.tools.model.entity.VoteQuestionEntity;
+import com.ssafy.team8alette.domain.bubble.tools.model.entity.VoteSelectEntity;
 
 @Repository
 public interface VoteSelectrRepository extends JpaRepository<VoteSelectEntity, VoteID> {
