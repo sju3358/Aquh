66da76cc10b2b969f6cd5e296a3eb68cd59f3458
tjdfkr011 <tjdfkr011@naver.com>
2023-08-11 15:30:08 +0900

    Feat : feed 목록 페이지 axios 수정

--- Front-End/aquh/src/components/users/NaverLoginButton.jsx
+++ Front-End/aquh/src/components/users/NaverLoginButton.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from "react";
 import classes from "./NaverLoginButton.module.css";
-
+import SERVER_URL from "../../config/SERVER_URL";
 export default function NaverLoginButton({}) {
   // TODO : .env 파일에 REACT_APP_NAVER_CLIENT_ID에 저장하기.
   const NAVER_CLIENT_ID = "sOLMKlOcIu3pqoFbtMey";
-  //TODO : 나중에 Redirect URI를 http://i9b108.p.ssafy.io:3000/ 로 바꾸기
-  //TODO : redirect URI를 main에서 닉네임 설정 화면으로 연결하기
-  const REDIRECT_URI_SERVER = "https://i9b108.p.ssafy.io/redirect";
-  const REDIRECT_URI_LOCAL = "https://localhost:3000/redirect";
-  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI_SERVER}`;
-  // const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI_LOCAL}`;
+
+  
+  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${SERVER_URL}/redirect`;
+  console.log(NAVER_AUTH_URL);
 
   const handleNaverLogin = () => {
     window.location.href = NAVER_AUTH_URL;
