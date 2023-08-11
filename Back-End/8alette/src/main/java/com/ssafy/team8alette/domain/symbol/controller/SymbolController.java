e3603c6894b2d84dd6d47d06bddd2cc35ed7ad28
GyuhoChoi <s4248942@gmail.com>
2023-08-11 10:59:54 +0900

    Fix : 마이페이지 획득한 심볼 수정

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/symbol/controller/SymbolController.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/symbol/controller/SymbolController.java
@@ -1,5 +1,7 @@
 package com.ssafy.team8alette.domain.symbol.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.team8alette.domain.symbol.model.dto.grant.response.AllSymbolListResponseDTO;
 import com.ssafy.team8alette.domain.symbol.model.dto.grant.response.GrantResponseDTO;
 import com.ssafy.team8alette.domain.symbol.model.service.SymbolGrantService;
 import com.ssafy.team8alette.domain.symbol.model.service.SymbolService;
@@ -30,12 +33,40 @@ public class SymbolController {
 	@GetMapping("/list/{memberNumber}")
 	public ResponseEntity<Map<String, Object>> getAllSymbol(@PathVariable Long memberNumber) {
 
+		List<AllSymbolListResponseDTO> symbolList = new ArrayList<>();
+
 		List<GrantResponseDTO> requiredSymbolList = symbolService.getRequiredSymbolList(memberNumber);
 		List<GrantResponseDTO> remainList = symbolService.getRemainList(memberNumber);
 
+		for (GrantResponseDTO requiredSymbol : requiredSymbolList) {
+			symbolList.add(new AllSymbolListResponseDTO(
+				requiredSymbol.getSymbolNumber(),
+				requiredSymbol.getSymbolName(),
+				requiredSymbol.getSymbolImgName(),
+				requiredSymbol.getSymbolCode(),
+				requiredSymbol.getSymbolConditionCnt(),
+				true, // acquiredActive
+				requiredSymbol.isSymbolActive()
+			));
+		}
+
+		for (GrantResponseDTO remainSymbol : remainList) {
+			symbolList.add(new AllSymbolListResponseDTO(
+				remainSymbol.getSymbolNumber(),
+				remainSymbol.getSymbolName(),
+				remainSymbol.getSymbolImgName(),
+				remainSymbol.getSymbolCode(),
+				remainSymbol.getSymbolConditionCnt(),
+				false, // acquiredActive
+				remainSymbol.isSymbolActive()
+			));
+		}
+
+		Collections.sort(symbolList,
+			(symbol1, symbol2) -> Long.compare(symbol1.getSymbolNumber(), symbol2.getSymbolNumber()));
+
 		Map<String, Object> responseData = new HashMap<>();
-		responseData.put("requiredSymbolList", requiredSymbolList);
-		responseData.put("remainSymbolList", remainList);
+		responseData.put("symbolList", symbolList);
 		responseData.put("message", "success");
 
 		return new ResponseEntity<>(responseData, HttpStatus.OK);
