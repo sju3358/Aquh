3e7fb21fb65f3ded4dfe97a65d98cf5cb348317d
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-11 14:50:30 +0900

    Feat : Https 수정

--- Front-End/aquh/src/utils/https.js
+++ Front-End/aquh/src/utils/https.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { errorSelector } from "recoil";
 
 let instance = axios.create({
   baseURL: "https://i9b108.p.ssafy.io",
@@ -27,33 +28,40 @@ instance.interceptors.response.use(
 
     const originalRequest = config;
 
-    if (status === 401) {
+    try {
+      if (status === 401) {
+        axios
+          .post(
+            "https://i9b108.p.ssafy.io/api/v1/member/auth/refresh_token",
+            {},
+            {
+              headers: {
+                "AUTH-TOKEN": localStorage.getItem("refresh_token"),
+              },
+            }
+          )
+          .then((response) => {
+            const newAccessToken = response.data.data.access_token;
+            const newRefreshToken = response.data.data.refresh_token;
 
-        axios.post("https://i9b108.p.ssafy.io/api/v1/member/auth/refresh_token",{},{
-          headers :{
-            "AUTH-TOKEN" : localStorage.getItem('refresh_token'),
-          }
-        })
-        .then((response) => {
-        
-          const newAccessToken = response.data.data.access_token;
-          const newRefreshToken = response.data.data.refresh_token;
+            localStorage.setItem("access_token", newAccessToken);
+            localStorage.setItem("refresh_token", newRefreshToken);
 
-          localStorage.setItem("access_token",newAccessToken);
-          localStorage.setItem("refresh_token",newRefreshToken);
-
-          originalRequest.headers = {
-            'Content-Type': 'application/json',
-            "AUTH-TOKEN" : newAccessToken,
-          };
-          return  axios(originalRequest);
-      })
-      .catch((error) =>{
-        return Promise.reject(error);
-      })
+            originalRequest.headers = {
+              "Content-Type": "application/json",
+              "AUTH-TOKEN": newAccessToken,
+            };
+            return axios(originalRequest);
+          })
+          .catch((error) => {
+            return Promise.reject(error);
+          });
+      }
+    } catch (error) {
+      new Error(error);
     }
+    return Promise.reject(error);
   }
-  
 );
 
 export default instance;
