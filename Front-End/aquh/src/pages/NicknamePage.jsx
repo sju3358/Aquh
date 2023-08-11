678fdc1e37fedeeae89f14faf9754c900f205ed2
seol <seolhyehyeon@gmail.com>
2023-08-11 21:54:47 +0900

    Add : 최초로그인 닉네임 설정

--- Front-End/aquh/src/pages/NicknamePage.jsx
+++ Front-End/aquh/src/pages/NicknamePage.jsx
@@ -3,13 +3,30 @@ import classes from "./NicknamePage.module.css";
 
 function NicknamePage() {
   return (
-    <div className={classes.container}>
-      <div className={classes.infoText}>
-        <img src='../../aquh3.png' alt='' className={classes.loginImg} />
-        <p>Aquh에 오신것을 환영합니다!</p>
-        <h3 className={classes.infoTextSmall}>
-          입장하기 전, 닉네임을 설정 해 주세요.
-        </h3>
+    <div className={classes.nicknameCheck}>
+      <div className={classes.container}>
+        <img src='../../aquh3.png' alt='' className={classes.nicknameImg} />{" "}
+        <div className={classes.infoText}>
+          <h1 className={classes.infoTextDetail}>
+            Aquh에 오신것을 환영합니다!
+          </h1>
+          <p className={classes.infoTextDetailSmall}>
+            입장하기 전, 방울의 이름을 지어 주세요.
+          </p>
+        </div>
+        <div className={classes.nicknameSection}>
+          <input
+            className={classes.nicknameInput}
+            type='text'
+            name='nickName'
+            id='nickName'
+            placeholder='불리고 싶은 이름을 적어주세요'
+          />
+          {/* TODO: 유효성검사, 중복확인 로직 필요 */}
+          <button className={classes.nicknameCheck}>중복확인</button>
+        </div>
+        {/* TODO: 입장하기 클릭->닉네임 변경(maybe 회원정보 수정?) axios.post-> 이메일 인증 요청 페이지(로그인 관문 3) */}
+        <button className={classes.enterBtn}>입장하기</button>
       </div>
       <input
         className={classes.nicknameInput}
