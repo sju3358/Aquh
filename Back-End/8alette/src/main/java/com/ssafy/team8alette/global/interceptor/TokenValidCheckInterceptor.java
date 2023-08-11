ff7c132b47846819c41deb34f9cc0b594ff1ff5f
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-11 17:46:11 +0900

    Feat : 토큰 기능 구현 완료에 따라 당분간 토큰체크 안하겠습니다.

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/global/interceptor/TokenValidCheckInterceptor.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/global/interceptor/TokenValidCheckInterceptor.java
@@ -1,14 +1,9 @@
 package com.ssafy.team8alette.global.interceptor;
 
-import java.util.Objects;
-
 import org.springframework.stereotype.Component;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.ssafy.team8alette.domain.member.auth.util.JwtTokenProvider;
-import com.ssafy.team8alette.global.annotation.LoginRequired;
-import com.ssafy.team8alette.global.exception.UnAuthorizedException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,23 +19,25 @@ public class TokenValidCheckInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
-		if (!(handler instanceof HandlerMethod)) {
-			return true;
-		}
+		return true;
 
-		HandlerMethod handlerMethod = (HandlerMethod)handler;
-		LoginRequired loginRequired = handlerMethod.getMethodAnnotation(LoginRequired.class);
-		if (Objects.isNull(loginRequired)) {
-			return true;
-		}
-
-		final String token = request.getHeader(HEADER_AUTH);
-
-		if (token != null) {
-			jwtTokenProvider.checkToken(token);
-			return true;
-		} else {
-			throw new UnAuthorizedException();
-		}
+		// if (!(handler instanceof HandlerMethod)) {
+		// 	return true;
+		// }
+		//
+		// HandlerMethod handlerMethod = (HandlerMethod)handler;
+		// LoginRequired loginRequired = handlerMethod.getMethodAnnotation(LoginRequired.class);
+		// if (Objects.isNull(loginRequired)) {
+		// 	return true;
+		// }
+		//
+		// final String token = request.getHeader(HEADER_AUTH);
+		//
+		// if (token != null) {
+		// 	jwtTokenProvider.checkToken(token);
+		// 	return true;
+		// } else {
+		// 	throw new UnAuthorizedException();
+		// }
 	}
 }
