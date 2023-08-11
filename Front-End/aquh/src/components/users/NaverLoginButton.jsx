c3dda21dbcf85892165e5538c522da9634860486
Jiyoung Park <parkninni@icloud.com>
2023-08-11 15:26:00 +0900

    Fix : Math.round로 fillPercent 반올림

--- Front-End/aquh/src/components/users/NaverLoginButton.jsx
+++ Front-End/aquh/src/components/users/NaverLoginButton.jsx
@@ -8,8 +8,8 @@ export default function NaverLoginButton({}) {
   //TODO : redirect URI를 main에서 닉네임 설정 화면으로 연결하기
   const REDIRECT_URI_SERVER = "https://i9b108.p.ssafy.io/redirect";
   const REDIRECT_URI_LOCAL = "https://localhost:3000/redirect";
-  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI_SERVER}`;
-  // const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI_LOCAL}`;
+  // const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI_SERVER}`;
+  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI_LOCAL}`;
 
   const handleNaverLogin = () => {
     window.location.href = NAVER_AUTH_URL;
