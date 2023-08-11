1b358f3a455bd6e676691d2384949b603cc5041d
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-11 21:35:57 +0900

    Feat : 마이페이지 피드 재활용으로 수정

--- Front-End/aquh/src/components/feed/UserFeedList.jsx
+++ Front-End/aquh/src/components/feed/UserFeedList.jsx
@@ -1,46 +1,152 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useRecoilValue } from 'recoil';
-import { memberNumberState } from '../../store/loginUserState';
-import { fetchFeedListByUser } from '../../utils/api/api.auth_service';
-import UserFeedCard from './UserFeedCard';
+import React from "react";
+import { useEffect, useState } from "react";
+import https from "../../utils/https";
+import classes from "../../pages/FeedPage.module.css";
+import FeedCard from "./FeedCard";
+import FeedModal from "./FeedModal";
 
 export default function UserFeedList() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [clickFeedData, setClickFeedData] = useState();
 
-  const [userFeeds, setUserFeeds] = useState([]);
-  
+  let feedList = [
+    {
+      feedNumber: 4,
+      feedCreatorNumber: 2,
+      title: "테스트3",
+      content: "테스트3",
+      feedLikeCnt: 0,
+      viewCnt: 0,
+      feedActive: true,
+      feedImgOrigin: null,
+      feedImgTrans: null,
+      createDate: "2023-08-11T12:25:02.653+00:00",
+      deleteDate: null,
+      exp: 180,
+      symbolLink: [],
+      followingCnt: 0,
+      nickName: "N_가나다초콜릿",
+      level: 1,
+    },
+    {
+      feedNumber: 3,
+      feedCreatorNumber: 2,
+      title: "테스트2",
+      content: "테스트2",
+      feedLikeCnt: 0,
+      viewCnt: 0,
+      feedActive: true,
+      feedImgOrigin: null,
+      feedImgTrans: null,
+      createDate: "2023-08-11T12:24:59.716+00:00",
+      deleteDate: null,
+      exp: 180,
+      symbolLink: [],
+      followingCnt: 0,
+      nickName: "N_가나다초콜릿",
+      level: 1,
+    },
+    {
+      feedNumber: 2,
+      feedCreatorNumber: 2,
+      title: "테스트1",
+      content: "테스트1",
+      feedLikeCnt: 0,
+      viewCnt: 0,
+      feedActive: true,
+      feedImgOrigin: null,
+      feedImgTrans: null,
+      createDate: "2023-08-11T12:24:56.881+00:00",
+      deleteDate: null,
+      exp: 180,
+      symbolLink: [],
+      followingCnt: 0,
+      nickName: "N_가나다초콜릿",
+      level: 1,
+    },
+    {
+      feedNumber: 1,
+      feedCreatorNumber: 2,
+      title: "테스트1",
+      content: "테스트2",
+      feedLikeCnt: 0,
+      viewCnt: 0,
+      feedActive: true,
+      feedImgOrigin: null,
+      feedImgTrans: null,
+      createDate: "2023-08-11T12:24:51.725+00:00",
+      deleteDate: null,
+      exp: 180,
+      symbolLink: [],
+      followingCnt: 0,
+      nickName: "N_가나다초콜릿",
+      level: 1,
+    },
+  ];
+
   useEffect(() => {
-    const fetchFeedList = async () => {
-      const response = await fetchFeedListByUser();
-      try{
-        const res = response.data.feedList;
-        // console.log(res)
-        setUserFeeds(res)
-      }
-      catch(error){
-      
-        console.log(error)
-      }
-    }; 
-    fetchFeedList();
+    https
+      .get("api/v1/feed")
+      .then((res) => {
+        feedList = res.data.feedList;
+        console.log(feedList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
-  const userFeedCards = userFeeds?.map((e) => 
-    <UserFeedCard
-      key={e.feedNumber}
-      userNumber={e.feedCreatorNumber}
-      title={e.title}
-      content={e.content}
-      nickname={e.nickName}
-      date={e.createDate}
-      character={e.characterName}
-      />       
-  )
-
   return (
-    <div>
-      {userFeedCards}
+    <div className={classes.feedPage}>
+      <div className={classes.feedListSection}>
+        <div className={classes.feedListNew}>
+          <div>
+            {true ? (
+              <div className={classes.feedListNew}>
+                <p className={classes.feedMent}>
+                  <img
+                    src="../../droplet-white.png"
+                    alt="droplet"
+                    className={classes.droplet}
+                  />
+                  대충 내가 뿌린 피드라는 뜻
+                </p>
+                <div>
+                  {feedList.map((feed) => {
+                    return (
+                      <div
+                        className={classes.newFeedCard}
+                        key={feed.feedNumber}
+                      >
+                        <FeedCard
+                          feedTitle={feed.title}
+                          feedContent={feed.content}
+                          feedCreateDate={feed.createDate}
+                          feedImage={feed.feedImgTrans}
+                          feedNumber={feed.feedNumber}
+                          userNickName={feed.nickName}
+                          setModalOpen={setModalOpen}
+                          // setClickedFeedNum={setClickedFeedNum}
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+                <FeedModal
+                  modalOpen={modalOpen}
+                  setModalOpen={setModalOpen}
+                  clickFeedData={clickFeedData}
+                />
+              </div>
+            ) : null}
+          </div>
+          <FeedModal
+            modalOpen={modalOpen}
+            setModalOpen={setModalOpen}
+            clickFeedData={clickFeedData}
+          />
+        </div>
+      </div>
     </div>
   );
 }
-
