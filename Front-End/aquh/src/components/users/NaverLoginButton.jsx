e2d83c201a194c622a49040db1bb5970f0d070bd
sju3358 <tjdfkr011.dev@gmail.com>
2023-08-11 15:59:46 +0900

    Feat : .env 수정

--- Front-End/aquh/src/components/users/NaverLoginButton.jsx
+++ Front-End/aquh/src/components/users/NaverLoginButton.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect } from "react";
 import classes from "./NaverLoginButton.module.css";
-import SERVER_URL from "../../config/SERVER_URL";
 export default function NaverLoginButton({}) {
-  // TODO : .env 파일에 REACT_APP_NAVER_CLIENT_ID에 저장하기.
-  const NAVER_CLIENT_ID = "sOLMKlOcIu3pqoFbtMey";
 
-  
-  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${SERVER_URL}/redirect`;
+   
+  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${process.env.REACT_APP_LOGIN_SERVER_URL}/redirect`;
   console.log(NAVER_AUTH_URL);
 
   const handleNaverLogin = () => {
