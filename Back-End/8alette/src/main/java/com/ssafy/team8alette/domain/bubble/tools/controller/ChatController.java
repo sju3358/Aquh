16fac889425df2f0e51e90a20b0fa9135242ff46
Jade_Good <kangsm423@gmail.com>
2023-08-14 00:51:24 +0900

    Fix : chat Controller - get

--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/controller/ChatController.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/controller/ChatController.java
@@ -1,15 +1,11 @@
 package com.ssafy.team8alette.domain.bubble.tools.controller;
 
-import com.ssafy.team8alette.domain.bubble.tools.model.dto.response.ChatResponseDTO;
+import com.ssafy.team8alette.domain.bubble.tools.model.dto.response.Chat;
 import com.ssafy.team8alette.domain.bubble.tools.service.ChatService;
 import com.ssafy.team8alette.domain.member.auth.util.JwtTokenProvider;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @RestController
@@ -40,25 +37,20 @@ public class ChatController {
 
     @CrossOrigin
     @GetMapping(value = "/{bubble_number}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ChatResponseDTO> getAllChatLog(
-            @PathVariable Long bubble_number
-    ) {
+    public Flux<Chat> getAllChatLog(
+            @PathVariable Long bubble_number) {
+
         return chatService.getChatLog(bubble_number);
     }
 
     @CrossOrigin
     @PostMapping
-    public ResponseEntity<Map<String, Object>> setMsg(
+    public Mono<Chat> setMsg(
             @RequestHeader(value = "AUTH-TOKEN") String jwtToken,
-            @RequestBody ChatResponseDTO chatResponseDTO) throws ParseException {
+            @RequestBody Chat chat) throws ParseException {
 
         Long member_number = jwtTokenProvider.getMemberNumber(jwtToken);
 
-        Map<String, Object> responseData = new HashMap<>();
-        responseData.put("message", "채팅 입력 성공");
-        responseData.put("status", 200);
-        responseData.put("result", chatService.insertChat(chatResponseDTO, member_number));
-
-        return new ResponseEntity<>(responseData, HttpStatus.OK);
+        return chatService.insertChat(chat, member_number);
     }
 }
--- Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/model/dto/response/Chat.java
+++ Back-End/8alette/src/main/java/com/ssafy/team8alette/domain/bubble/tools/model/dto/response/Chat.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "chat")
-public class ChatResponseDTO {
+public class Chat {
     @Id
     private String id;
     private String msg;
