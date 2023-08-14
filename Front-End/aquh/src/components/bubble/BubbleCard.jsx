4e4e1245ad752dd51d2307ca77f7866b0c5493dc
GyuhoChoi <s4248942@gmail.com>
2023-08-14 16:42:05 +0900

    style : 버블 참여하기 css 적용

--- Front-End/aquh/src/components/bubble/BubbleCard.jsx
+++ Front-End/aquh/src/components/bubble/BubbleCard.jsx
@@ -4,42 +4,48 @@ import Capsule from "../ui/Capsule";
 import { bubbleList } from "../../utils/api/api.bubble_service";
 import Button from "../ui/Button";
 import { FaRegCalendarCheck } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function BubbleCard({
-  title = '방제목',
-  content= '방내용',
-  thumbnail = 'https://picsum.photos/200/300',
-  type = '방타입',
-  category = '카테고리',
-  host = '호스트번호',
-  openDate = '2023-08-12T18:33:56.36681',
-  closeDate = '2023-08-12T18:33:56.34344',
+  title = "방제목",
+  content = "방내용",
+  thumbnail = "https://picsum.photos/200/300",
+  type = "방타입",
+  category = "카테고리",
+  host = "호스트번호",
+  openDate = "2023-08-12T18:33:56.36681",
+  closeDate = "2023-08-12T18:33:56.34344",
   bubbleId,
-  onJoin = () => {}
-}) 
-
-{
+  onJoin = () => {},
+}) {
   const id = bubbleId;
   return (
     <div className={classes.card}>
       {thumbnail && (
-        <img className={classes.cardImgTop} src={thumbnail} alt='Event' />
+        <img className={classes.cardImgTop} src={thumbnail} alt="Event" />
       )}
       {/* TODO: roomType */}
       {/* <Capsule /> */}
       <p className={classes.eventTitle}>{title}</p>
       <p className={classes.eventDate}>
         {" "}
-        <FaRegCalendarCheck /> {openDate?.slice(0, 10)} ~{" "} {closeDate?.slice(0, 10)}
+        <FaRegCalendarCheck /> {openDate?.slice(0, 10)} ~{" "}
+        {closeDate?.slice(0, 10)}
       </p>
       <div>{type}</div>
-      <Button onClick={onJoin} variant='regular' className='card-button'>
+      <button onClick={onJoin} className={classes.cardButton}>
         {/* 참여하기 */}
         {/* 0807 김재원 수정 */}
         {/*  */}
-        <Link to={`/bubble/${id}`}> 디테일페이지로 </Link>
-      </Button>
+        <Link
+          to={`/bubble/${id}`}
+          style={{ color: "white" }}
+          className="detail-link"
+        >
+          {" "}
+          참여하기{" "}
+        </Link>
+      </button>
     </div>
   );
 }
