4106b9eabf3b922e0db60c658b70fbd99b39114e
Jade_Good <kangsm423@gmail.com>
2023-08-17 02:47:26 +0900

    Fix : openvidu - camera, mic toggle

--- Front-End/aquh/src/components/video/OvVideo.js
+++ Front-End/aquh/src/components/video/OvVideo.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import classes from "./UserVideoComponent.module.css";
 
 export default class OpenViduVideoComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
 
-    constructor(props) {
-        super(props);
-        this.videoRef = React.createRef();
+  componentDidUpdate(props) {
+    if (props && !!this.videoRef) {
+      this.props.streamManager.addVideoElement(this.videoRef.current);
     }
+  }
 
-    componentDidUpdate(props) {
-        if (props && !!this.videoRef) {
-            this.props.streamManager.addVideoElement(this.videoRef.current);
-        }
+  componentDidMount() {
+    if (this.props && !!this.videoRef) {
+      this.props.streamManager.addVideoElement(this.videoRef.current);
     }
+  }
 
-    componentDidMount() {
-        if (this.props && !!this.videoRef) {
-            this.props.streamManager.addVideoElement(this.videoRef.current);
-        }
-    }
-
-    render() {
-        return <video autoPlay={true} ref={this.videoRef} />;
-    }
-
+  render() {
+    return <video className={classes.videoBox} autoPlay={true} ref={this.videoRef} />;
+  }
 }
