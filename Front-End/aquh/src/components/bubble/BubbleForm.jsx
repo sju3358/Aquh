11ac006c5df1d96e9b24341f44668d619b09368d
Kanaloa21 <kjwkjw1104@gmail.com>
2023-08-16 13:23:42 +0900

    Fix: prompt 수정

--- Front-End/aquh/src/components/bubble/BubbleForm.jsx
+++ Front-End/aquh/src/components/bubble/BubbleForm.jsx
@@ -5,7 +5,6 @@ import { memberNumberState } from "../../store/loginUserState";
 import classes from "./BubbleForm.module.css";
 import BubbleImage from "./BubbleImage";
 
-
 export default function BubbleForm({ onSubmit }) {
   const memberNumber = useRecoilValue(memberNumberState);
   const [bubbleForm, setBubbleForm] = useState({
@@ -13,11 +12,11 @@ export default function BubbleForm({ onSubmit }) {
     bubbleTitle: "",
     bubbleContent: "",
     bubbleThumbnail: "",
-    categoryNumber : "",
+    categoryNumber: "",
     // planOpenDate: '',
     // planCloseDate: '',
   });
-  
+
   const [selectedValue, setSelectedValue] = useState("6");
 
   const handleSelectedOption = (e) => {
@@ -27,11 +26,12 @@ export default function BubbleForm({ onSubmit }) {
       ...bubbleForm,
       categoryNumber: newValue,
     });
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(bubbleForm);
+    console.log(selectedValue, "전달 안되고 난리야");
   };
 
   const handleChange = (e) => {
@@ -44,37 +44,38 @@ export default function BubbleForm({ onSubmit }) {
   };
 
   const handleThumbnailSubmit = async (form) => {
-    await axios.post('https://api.kakaobrain.com/v2/inference/karlo/t2i',
-      {
-        "prompt" : `Sticker, ${form.bubbleImagePrompt}, line art drawing, ultra-detailed, clean Line art, full body, fine thin Lines, white background,`,
-        "negative_prompt" : "out of frame, people, person, nsfw, close up, cropped, bad proportions, ugly, disfigured, blurry, pixelated, hideous, indistinct, out of frame, text, letters, watermark",
-        "num_inference_steps" : 17,
-        "guidance_scale" : 20,
-        "nsfw_checker" : true,
-      },
-      {
-        headers: {
-          "Authorization" : "KakaoAK 5e3cba8892bdddabf18a9f685ea9fead",
-          "Content-Type" : "application/json",
+    await axios
+      .post(
+        "https://api.kakaobrain.com/v2/inference/karlo/t2i",
+        {
+          prompt: `${form.bubbleImagePrompt}, ultra-detailed, white background, 3D, Object, miniature`,
+          negative_prompt:
+            "out of frame, people, person, nsfw, close up, cropped, bad proportions, ugly, disfigured, blurry, pixelated, hideous, indistinct, out of frame, text, letters, watermark",
+          prior_num_inference_steps: 23,
+          prior_guidance_scale: 15,
+          nsfw_checker: true,
         },
-      }
-    ).then(
-      (response) => {
+        {
+          headers: {
+            Authorization: "KakaoAK 5e3cba8892bdddabf18a9f685ea9fead",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(` ${form.bubbleImagePrompt}`);
         setBubbleForm({
           ...bubbleForm,
           bubbleThumbnail: response.data.images[0].image,
         });
-        console.log("썸네일 생성 성공", response.data)
-      }
-    ).catch(
-      (error) => {
-        console.log("썸네일 생성 실패", error)
-      }
-    )
-    
-  }
+        console.log("썸네일 생성 성공", response.data.images[0].image);
+      })
+      .catch((error) => {
+        console.log("썸네일 생성 실패", error);
+      });
+  };
 
-  console.log("selected", selectedValue)
+  console.log("selected", selectedValue);
 
   const { bubbleTitle, bubbleContent, bubbleThumbnail } = bubbleForm;
   return (
@@ -85,8 +86,8 @@ export default function BubbleForm({ onSubmit }) {
           이미지
         </label>
         <div className={classes.inputbox}>
-          <img 
-            src={bubbleThumbnail} 
+          <img
+            src={bubbleThumbnail}
             value={bubbleThumbnail}
             onChange={handleChange}
           ></img>
@@ -117,16 +118,21 @@ export default function BubbleForm({ onSubmit }) {
           />
         </div>
         <label htmlFor="category">카테고리</label>
-          <select size="1" id="category" onChange={handleSelectedOption} value={selectedValue}>
-            <option value="1">스포츠-운동</option>
-            <option  value="2">수공예-드로잉</option>
-            <option  value="3">요리-베이킹</option>
-            <option  value="4">문화-예술</option>
-            <option  value="5">미용-뷰티</option>
-            <option  value="6">홈-리빙</option>
-            <option  value="7">자기개발</option>
-            <option  value="8">기타</option>
-          </select> 
+        <select
+          size="1"
+          id="category"
+          onChange={handleSelectedOption}
+          value={selectedValue}
+        >
+          <option value="1">스포츠-운동</option>
+          <option value="2">수공예-드로잉</option>
+          <option value="3">요리-베이킹</option>
+          <option value="4">문화-예술</option>
+          <option value="5">미용-뷰티</option>
+          <option value="6">홈-리빙</option>
+          <option value="7">자기개발</option>
+          <option value="8">기타</option>
+        </select>
         <div className={classes.buttonContainer}>
           <button className={classes.button}>제출하기</button>
         </div>
