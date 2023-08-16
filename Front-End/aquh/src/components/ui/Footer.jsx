5cf28b5f6eae92124eb687144dfbf8e5f84d92c5
Jiyoung Park <parkninni@icloud.com>
2023-08-16 17:28:41 +0900

    Style : 스크롤바 스타일 적용

--- Front-End/aquh/src/components/ui/Footer.jsx
+++ Front-End/aquh/src/components/ui/Footer.jsx
@@ -1,3 +1,9 @@
+import classes from './Footer.module.css'
+
 export default function Footer(){
-  return <></>
+  return (
+  <footer className={classes.footer}>
+    <p>© 2021 - Todos os direitos reservados</p>
+  </footer>
+  )
 }
