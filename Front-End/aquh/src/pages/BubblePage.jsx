b3d0506adad09fcbf3efa9551ac3620368e57166
GyuhoChoi <s4248942@gmail.com>
2023-08-16 13:19:26 +0900

    Style : bubbleCard css 변경

--- Front-End/aquh/src/pages/BubblePage.jsx
+++ Front-End/aquh/src/pages/BubblePage.jsx
@@ -1,5 +1,9 @@
 import classes from "./BubblePage.module.css";
-import { bubbleList, joinedBubbleList, bubbleCategory } from "../utils/api/api.bubble_service";
+import {
+  bubbleList,
+  joinedBubbleList,
+  bubbleCategory,
+} from "../utils/api/api.bubble_service";
 import { useEffect, useState } from "react";
 import ButtonSelector from "../components/ui/ButtonSelector";
 import BubbleList from "../components/bubble/BubbleList";
@@ -7,17 +11,13 @@ import Modal from "../components/ui/Modal";
 import BubbleForm from "../components/bubble/BubbleForm";
 import https from "../utils/https";
 
-
-
 export default function BubblePage() {
-
   const [bubbles, setBubbles] = useState([]);
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedType, setSelectedType] = useState(null);
   const [joinedBubbles, setJoinedBubbles] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
 
   // fetch CategoryList
   useEffect(() => {
@@ -26,29 +26,27 @@ export default function BubblePage() {
         const response = await bubbleCategory();
         const res = response.data.category;
         setCategories(res);
-      }
-      catch(error){
+      } catch (error) {
         console.log(`Oh nonono BubblePage! ${error}`);
-      } 
-    }
+      }
+    };
     fetchBubbleCategory();
-  }, [])
+  }, []);
 
-  // fetch BubbleList 
+  // fetch BubbleList
   useEffect(() => {
     const fetchBubbleList = async () => {
       try {
         const response = await bubbleList();
         const res = response.data.data;
-        console.log("bubbleList", res)
-        setBubbles(res)
-      }
-      catch(error){
+        console.log("bubbleList", res);
+        setBubbles(res);
+      } catch (error) {
         console.log(`Oh nonono BubblePage! ${error}`);
       }
-    }
+    };
     fetchBubbleList();
-  }, [])
+  }, []);
 
   //fetch JoinedBubbleList
   useEffect(() => {
@@ -56,52 +54,33 @@ export default function BubblePage() {
       try {
         const response = await joinedBubbleList();
         const res = response.data.data;
-        console.log("joinedBubbleList", res)
-        setJoinedBubbles(res)
+        console.log("joinedBubbleList", res);
+        setJoinedBubbles(res);
+      } catch (error) {
+        console.log(error);
       }
-      catch(error){
-        console.log(error)
-      }
-      }
-      fetchJoinedBubbleList();
-    }, [])
+    };
+    fetchJoinedBubbleList();
+  }, []);
 
-  const categoryNames = categories
-    .map(category => category.categoryName)
+  const categoryNames = categories.map((category) => category.categoryName);
 
   // related Modal
   const showModal = () => {
-    setIsModalOpen(prev => !prev)
-  }
+    setIsModalOpen((prev) => !prev);
+  };
 
-
-
-// post 버블 생성
-const handleFormSubmit = (form) => {
-  https.post('api/v1/bubble', form)
-    .then(
-      (response) => {
-        console.log("방생성 성공", response)
-      }
-    ).then(
-      https.post(`/api/v1/bubble-session/${form.bubbleNumber}`, form)
-        .then(
-          (response)=> {
-            console.log("화상생성 성공", response)
-          }
-      ).catch(
-        (error) => {
-          console.log("화상생성 실패", error)
-        }
-      )
-    )
-    .catch(
-      (error) => {
-        console.log("방생성 실패", error)
-      }
-  )
-}
-
+  // post 버블 생성
+  const handleFormSubmit = (form) => {
+    https
+      .post("api/v1/bubble", form)
+      .then((response) => {
+        console.log("방생성 성공", response);
+      })
+      .catch((error) => {
+        console.log("방생성 실패", error);
+      });
+  };
 
   return (
     <div className={classes.container}>
@@ -109,52 +88,54 @@ const handleFormSubmit = (form) => {
         variant="regular"
         initiallySelected="전체"
         options={["전체", ...categoryNames]}
-        onSelect={category => setSelectedCategory(category)}/>
+        onSelect={(category) => setSelectedCategory(category)}
+      />
 
       <p className={classes.latestMent}>
         <img
-          src='../../droplet-white.png'
-          alt='droplet'
+          src="../../droplet-white.png"
+          alt="droplet"
           className={classes.droplet}
         />
         현재 참여중인 버블이예요
       </p>
       <div className={classes.latestChat}>
-        <BubbleList 
-        bubbles={joinedBubbles}
-        selectedCategory={selectedCategory}
-        selectedType={selectedType}
+        <BubbleList
+          bubbles={joinedBubbles}
+          selectedCategory={selectedCategory}
+          selectedType={selectedType}
         />
       </div>
       <p className={classes.latestMent}>
         <img
-          src='../../droplet-white.png'
-          alt='droplet'
+          src="../../droplet-white.png"
+          alt="droplet"
           className={classes.droplet}
         />
         Aquh에서 새로운 버블들을 찾아보세요
       </p>
       <div className={classes.buttonContainer}>
-      <ButtonSelector
-        variant="alternate"
-        initiallySelected="전체"
-        options={["전체", "버블링", "버블톡"]}
-        onSelect={type => setSelectedType(type)} />
-      <button onClick={showModal} className={classes.createRoom}>+</button>
+        <ButtonSelector
+          variant="alternate"
+          initiallySelected="전체"
+          options={["전체", "버블링", "버블톡"]}
+          onSelect={(type) => setSelectedType(type)}
+        />
+        <button onClick={showModal} className={classes.createRoom}>
+          +
+        </button>
       </div>
-      <Modal isModalOpen={isModalOpen} onClose={() => setIsModalOpen(false)}> 
+      <Modal isModalOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <BubbleForm onSubmit={handleFormSubmit} />
       </Modal>
 
       <div className={classes.oldChat}>
-         <BubbleList
+        <BubbleList
           bubbles={bubbles}
           selectedCategory={selectedCategory}
-          selectedType={selectedType} 
-          /> 
-         
+          selectedType={selectedType}
+        />
       </div>
     </div>
   );
 }
-    
