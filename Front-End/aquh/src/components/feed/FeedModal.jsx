6e0228d5ea594cdc00007cc0f48d0cca2ccbaf1f
GyuhoChoi <s4248942@gmail.com>
2023-08-17 15:47:56 +0900

    Style : FeedModal.jsx 수정

--- Front-End/aquh/src/components/feed/FeedModal.jsx
+++ Front-End/aquh/src/components/feed/FeedModal.jsx
@@ -6,6 +6,8 @@ import { useRecoilValue } from "recoil";
 import { memberNumberState } from "../../store/loginUserState";
 import emptyHeart from "../../assets/emptyHeart.png";
 import fullHeart from "../../assets/fullHeart.png";
+import { GoArrowUpRight } from "react-icons/go";
+import { FiFilePlus } from "react-icons/fi";
 
 export default function FeedModal({ setModalOpen, modalOpen, clickFeedData }) {
   const [isModify, setIsModify] = useState();
@@ -113,18 +115,21 @@ export default function FeedModal({ setModalOpen, modalOpen, clickFeedData }) {
     },
     // 모달창 내부 관련 스타일링
     content: {
+      // width: "80rem",
       display: "flex",
+      justifyContent: "center",
       flexDirextion: "column",
-      backgroundColor: "rgba(255,255,255,0.95)",
+      backgroundColor: "#ffffff",
       overflow: "auto",
       zIndex: 10,
       // top,left,right,bottom ==> 모달창의 사이즈가 아니고 여백에서부터
       // 얼마나 떨어지게 할지 입니다.
-      top: "150px",
-      left: "300px",
-      right: "300px",
-      bottom: "150px",
+      top: "100px",
+      left: "420px",
+      right: "420px",
+      bottom: "100px",
       // border: "2px solid black",
+
       borderRadius: "20px",
     },
   };
@@ -144,15 +149,13 @@ export default function FeedModal({ setModalOpen, modalOpen, clickFeedData }) {
     >
       {isModify ? (
         <div className={classes.feedWriteCard}>
-          <div className={classes.feedTitle}>
-            <input
-              type="text"
-              value={feedTitle}
-              className={classes.feedTitle}
-              onChange={onChangeFeedTitle}
-              placeholder="제목을 입력하세요"
-            />
-          </div>
+          <input
+            type="text"
+            value={feedTitle}
+            className={classes.feedTitle}
+            onChange={onChangeFeedTitle}
+            placeholder="제목을 입력하세요"
+          />
 
           <textarea
             cols="80"
@@ -171,7 +174,10 @@ export default function FeedModal({ setModalOpen, modalOpen, clickFeedData }) {
               readOnly={true}
             />
             <label for="newFile" className={classes.feedFileLabel}>
-              파일찾기
+              <div className={classes.findFeedFile}>
+                <FiFilePlus />
+                &nbsp;&nbsp;사진
+              </div>
             </label>
             <input
               className={classes.feedImgRealBtn}
@@ -180,10 +186,16 @@ export default function FeedModal({ setModalOpen, modalOpen, clickFeedData }) {
               id="newFile"
               type="file"
             />
+            <button
+              className={classes.buttonRewrite}
+              onClick={onClinkModifyBtn}
+            >
+              <div className={classes.modifyFeedFile}>
+                <GoArrowUpRight />
+                &nbsp;&nbsp; 수정
+              </div>
+            </button>
           </div>
-          <button className={classes.buttonRewrite} onClick={onClinkModifyBtn}>
-            수정하기
-          </button>
         </div>
       ) : (
         // 모달 처음 클릭했을 때 Read 페이지
