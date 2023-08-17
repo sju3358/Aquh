c12ba3800e0db858df151a33ed548a5a228ade04
seol <seolhyehyeon@gmail.com>
2023-08-17 16:48:09 +0900

    Fix : 버블채팅방 화면 Css 수정

--- Front-End/aquh/src/components/video/UserVideoComponent.js
+++ Front-End/aquh/src/components/video/UserVideoComponent.js
@@ -21,9 +21,9 @@ export default class UserVideoComponent extends Component {
             <OpenViduVideoComponent streamManager={this.props.streamManager} />
             <div className={classes.videoProfile}>
               <img className={classes.videoProfileImg} src={`../../chat-profile${this.getUserLevel(this.props.streamManager.stream.connection.data)}.png`} alt="AvatarImg" />
-              <div className={classes.videoProfileNickName}>
+              <p className={classes.videoProfileNickName}>
                 {this.getUserNickname(this.props.streamManager.stream.connection.data)}
-              </div>
+              </p>
             </div>
           </div>
         ) : null}
